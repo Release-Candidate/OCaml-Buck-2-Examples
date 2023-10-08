049926d723141945243a8f99b66bd1ddca8594db
Release-Candidate <rec@gmx.at>
2023-10-08 10:16:51 +0200

    Initial code check-in
    Signed-off-by: Release-Candidate <rec@gmx.at>

---
+++ ocamllex_menhir_example/test/ocamllex_menhir_example.ml
@@ -0,0 +1,9 @@
+(*
+ SPDX-FileCopyrightText:  Copyright 2023 Roland Csaszar
+ SPDX-License-Identifier: MIT
+
+ Project:  OCaml-Buck-2-Examples, ocamllex_menhir_example
+ File:     ocamllex_menhir_example.ml
+ Date:     08.Oct.2023
+
+============================================================================== *)
